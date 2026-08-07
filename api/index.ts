73af51d21f40f4daa82f00ef043a54c379da1267
jason-bccsworld <Jason@bccsworld.com>
2026-08-07 21:31:14 +0000

    Include pdfjs worker files in serverless function bundle (v13)

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@
 import { createApp } from "./_server.mjs";
 import type { Express, Request, Response } from "express";
 
-const VERSION = "v12";
+const VERSION = "v13";
 
 type AppState =
   | { ready: true; app: Express }
